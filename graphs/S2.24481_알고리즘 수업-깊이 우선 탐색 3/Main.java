82350aa139b09df8699b3589a57783af5f381c14
siwon <muncool39@gmail.com>
2023-08-17 17:45:06 +0900

    Solve: graphs S2.24481
    title: 알고리즘 수업 - 깊이 우선 탐색 3
    tag:
    -dfs
    -graphs
    -graph_traversal
    -sorting

---
+++ graphs/S2.24481_알고리즘 수업-깊이 우선 탐색 3/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+    static int[] deep;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		arr = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		deep = new int[N+1];
+		for(int i=1;i<=N;i++){
+		    arr[i] = new ArrayList<Integer>();
+		}
+		for(int i=0;i<M;i++){
+		    st = new StringTokenizer(br.readLine()," ");
+		    int u = Integer.parseInt(st.nextToken());
+		    int v = Integer.parseInt(st.nextToken());
+		    arr[u].add(v);
+		    arr[v].add(u);
+		}
+		for(int i=1;i<=N;i++){
+		    Collections.sort(arr[i]);
+		}
+		dfs(R,0);
+		StringBuilder sb = new StringBuilder("");
+		for(int i=1;i<=N;i++){
+		    if(deep[i]==0 && i!=R) deep[i]=-1;
+		    sb.append(deep[i]).append("\n");
+		}
+		System.out.print(sb);
+	}
+	static void dfs(int i, int d){
+	    visited[i] = true;
+	    deep[i] = d;
+	    for(int u : arr[i]){
+	        if(visited[u]) continue;
+	        dfs(u,d+1);
+	    }
+	}
+}
