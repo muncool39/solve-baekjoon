061558f8776ac9c66c8b620d4c9c33f138963e6e
siwon <muncool39@gmail.com>
2026-03-01 15:30:26 +0900

    solve(mml): S5.9656 돌게임 2

---
+++ math/S5.9656_돌 게임 2/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+	    if(N%2 == 0) {
+	        System.out.print("SK");
+	    } else {
+	        System.out.print("CY");
+	    }
+	}
+}
